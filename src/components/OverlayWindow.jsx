d4a04bd84b3c1bd8d7e96e5d41952bf458f33d90
Sharktooth <22bcs50186@cuchd.in>
2024-09-16 10:58:20 +0530

    commit before final

--- src/components/OverlayWindow.jsx
+++ src/components/OverlayWindow.jsx
@@ -3,6 +3,10 @@
 import { X } from "lucide-react";
 import Draggable from "react-draggable";
 import { useEffect, useState } from "react";
+import st from "../../public/assets/12.jpg";
+import so from "../../public/assets/67.png";
+import sl from "../../public/assets/68.png";
+import si from "../../public/assets/70.png";
 import {
   Button,
   Frame,
@@ -62,10 +66,86 @@ export default function OverlayWindow({ app, onClose, position, index }) {
             </Button>
           </WindowHeader>
         </Frame>
-        <ScrollView className="h-[92%]">
-          <WindowContent className="p-4">
-            {app.name === "About us" && <>fdas</>}
-            <Image alt="about" src="/assets/1.png" width={1000} height={1000} />
+        <ScrollView className="h-[92%] overflow-hidden">
+          <WindowContent className="p-4 flex ">
+            {app.name === "About us" && (
+              <>
+                <div className="flex flex-col">
+                  <div className="  flex justify-center items-center">
+                    <div>
+
+                    <Image
+                      className="w-[30%] h-[70%]"
+                      alt="about"
+                      src={st}
+                      width={1000}
+                      height={1000}
+                      />
+                      </div>
+                    <div>
+                      <h1>
+                        Netscape Navigator<br></br> version 2.01
+                      </h1>
+                      <p>Copyright @ 1994-1995 baZarMash</p>
+                      <p>
+                        This software is subject to licence aggrements,
+                        Pleasease in the name of love ~ love me
+                      </p>
+                      <p>Report any problem through ur mom</p>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row gap-5 justify-between items-center">
+
+                    <div className="flex flex-col justify-center items-center">
+                      <Image
+                        className="w-[95%] h-[40%]"
+                        alt="about"
+                        src={sl}
+                        width={1000}
+                        height={1000}
+                      ></Image>
+                      <h1>Ayush aka </h1>
+                      <p>Full-Stack Dev</p>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center">
+                      <Image
+                        className="w-[95%] h-[40%]"
+                        alt="about"
+                        src={so}
+                        width={1000}
+                        height={1000}
+                      ></Image>
+                      <h1>Anukul</h1>
+                      <p>Front-End Designer</p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                      <Image
+                        className="w-[95%] h-[40%]"
+                        alt="about"
+                        src={si}
+                        width={1000}
+                        height={1000}
+                      ></Image>
+                      <h1>Satynash </h1>
+                      <p> Web3 Backend</p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                      <Image
+                        className="w-[95%] h-[40%]"
+                        alt="about"
+                        src={si}
+                        width={1000}
+                        height={1000}
+                      ></Image>
+                      <h1>Rahul </h1>
+                      <p>Web3 Backend</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
           </WindowContent>
         </ScrollView>
       </Window>
