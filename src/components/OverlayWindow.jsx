61150364bfff816088fa0432c1f70cce4ca16a11
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-09-18 14:32:34 +0530

    arranged leaderboard according to the ranking

--- src/components/OverlayWindow.jsx
+++ src/components/OverlayWindow.jsx
@@ -164,8 +164,8 @@ export default function OverlayWindow({
               <div className="w-full h-full flex flex-col gap-4">
                 <h1 className="text-center font-bold text-2xl">LeaderBoard</h1>
                 <ScrollView className="h-[80%]">
-                  <div className="w-full h-full px-3 flex">
-                    <div>
+                  <div className="w-full h-full px-3 flex flex-col">
+                    {/* <div>
                       <div className="font-semibold pb-2 text-center mr-[40%] ">
                         Token
                       </div>
@@ -208,14 +208,62 @@ export default function OverlayWindow({
                           </div>
                         ))}
                       </div>
+                    </div> */}
+
+                    <div className="w-full px-20  flex justify-center ">
+                      <div className="font-semibold pb-2 text-center  w-[40%] ">
+                        Token
+                      </div>
+                      <div className="font-semibold  pb-2 text-center pl-20  w-[30%] ">
+                        Ranking
+                      </div>
+                      <div className="font-semibold pb-2  pl-28 text-center w-[30%] ">
+                        Images
+                      </div>
+                    </div>
+                    <div>
+                      {Object.entries(TokenScores)
+                        .sort(([, a], [, b]) => b - a) 
+                        .map(([key, value]) => (
+                          <div
+                            key={key}
+                            className="w-full flex items-center justify-between gap-4 py-4"
+                          >
+                            <div className="w-[50%]">
+                              <a
+                                href={`https://bazar.ar.io/#/asset/${key}`}
+                                className="text-blue-500 underline w-[90%]"
+                              >
+                                {key.length > 30
+                                  ? `${key.substring(0, 30)}...`
+                                  : key}
+                              </a>
+                            </div>
+
+                            <div className="w-[15%] text-center">{value}</div>
+
+                            <div className="w-[30%] flex justify-center">
+                              <div
+                                className="size-14 bg-contain pl-10"
+                                style={{
+                                  backgroundImage: `url(https://arweave.net/${key})`,
+                                }}
+                              />
+                            </div>
+                          </div>
+                        ))}
                     </div>
                   </div>
                 </ScrollView>
               </div>
             )}
-            {
-              app.name==="Bazar" && (<h1><Link target="_blank" href="https://bazar.arweave.dev" >Visit Bazar</Link></h1>)
-            }
+            {app.name === "Bazar" && (
+              <h1>
+                <Link target="_blank" href="https://bazar.arweave.dev">
+                  Visit Bazar
+                </Link>
+              </h1>
+            )}
           </WindowContent>
         </ScrollView>
       </Window>
